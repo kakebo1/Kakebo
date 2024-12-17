f6199c09bacc1ec87d4b69e9c9a122c17991adaa
kakebo1 <kakeboproject@gmail.com>
2024-12-17 15:12:29 -0600

    Se agregaron las dependencias correspondientes de Kotlin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     //id("com.android.application")
     id("com.google.gms.google-services")
+    kotlin("android")
 }
 
 android {
@@ -37,9 +38,12 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(libs.kotlin.gradle.plugin)
 
+    // Dependencia necesaria para scripts Kotlin
+    implementation(libs.kotlin.script.runtime)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -53,4 +57,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    kotlin("script-runtime")
+    kotlin("jvm") version "1.8.20" apply false
     alias(libs.plugins.android.application) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+}
+dependencies {
+
 }
