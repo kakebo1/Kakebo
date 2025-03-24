7ae4f6afdb925523a40324fa71437cc8778e1e2f
MicraBelmont <belmonttrevor04@gmail.com>
2025-03-24 12:57:24 -0600

    Prueba

--- app/src/main/java/com/example/myapplication/LlenadoPlanIngresos.java
+++ app/src/main/java/com/example/myapplication/LlenadoPlanIngresos.java
@@ -19,6 +19,7 @@ public class LlenadoPlanIngresos extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+            //Hola
         });
     }
 }
